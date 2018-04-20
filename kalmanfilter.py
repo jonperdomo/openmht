849a96a488a4d5f9d1e61ce479633ba9b8e41bec
Jonathan Perdomo <perdomojonathan5@gmail.com>
2018-04-19 20:43:32 -0400

    Added multivariate normal distribution likelihood, and motion scores for individual tracks.

--- kalmanfilter.py
+++ kalmanfilter.py
@@ -9,14 +9,12 @@ class KalmanFilter:
     def __init__(self, initial_observation):
         # For 2D X & Y pixel positions
         print("initial observation: {}".format(initial_observation))
+        print("initial observation shape: {}".format(np.matrix(initial_observation).T.shape))
         self.z = [np.matrix(initial_observation).T]  # observations
-        # self.Q = 1e-5  # process variance
         self.Q = np.matrix(np.eye(2)) * 1e-5
 
         # allocate space for arrays
         self.xhat = []  # a posteri estimate of x
-        # self.P = []  # a posteri error estimate
-        # self.P = []  # a posteri error estimate
         self.P = [np.matrix(np.eye(2))]  # a posteri error estimate
         self.xhatminus = []  # a priori estimate of x
         self.Pminus = []  # a priori error estimate
@@ -24,15 +22,16 @@ class KalmanFilter:
         self.R = 0.1 ** 2  # estimate of measurement variance, change to see effect
 
         # initial guesses
-        self.xhat.append(initial_observation)
-        # self.P.append(np.repeat(1.0, len(initial_observation)))
-        self.k = 0  # iteration
+        self.xhat.append(self.z[-1])
 
     def update(self, z):
         self.z.append(z)
-        print("\nPredicted position (mu): {}".format(self.xhat[-1]))
-        print("Actual position: {}".format(z))
-        print("Covariance (sigma): {}".format(self.P[-1]))
+        print("\n\nPredicted position (mu): {}".format(self.xhat[-1]))
+        print("\n\nActual position: {}".format(z))
+        print("\n\nCovariance (sigma): {}".format(self.P[-1]))
+
+        # get the likelihood score
+        L = self.calculate_likelihood(z, self.xhat[-1], self.P[-1])
 
         # time update
         self.xhatminus.append(self.xhat[-1])
@@ -41,13 +40,41 @@ class KalmanFilter:
         # measurement update
         self.K.append(self.Pminus[-1] / (self.Pminus[-1] + self.R))
         self.xhat.append(self.xhatminus[-1] + self.K[-1] * (self.z[-1] - self.xhatminus[-1]))
-        self.P.append((1 - self.K[-1]) * self.Pminus[-1])
+        self.P.append((1.0 - self.K[-1]) * self.Pminus[-1])
+
+        return L
 
     def calculate_likelihood(self, x, mu, sigma):
         """
         Likelihood function for a multivariate normal distribution.
         https://en.wikipedia.org/wiki/Multivariate_normal_distribution#Likelihood_function
         """
-        k = 2  # dimensions
-        L = -0.5(np.log(np.abs(sigma))) + (x-mu).T * np.linalg.inv(sigma) * (x-mu) + k * np.log(2*math.pi)
-        print("Likelihood: {}".format(L))
+        x = np.squeeze(x)
+        mu = np.array(mu).ravel('F')
+        if mu.shape != (2,):
+            print("Incorrect dimensions for mu: {}".format(mu.shape))
+            print("\n\nmu: {}".format(mu))
+            return 1.0
+        sigma = np.matrix(sigma).reshape((2, 2))
+        print("\n\nmu: {}".format(mu))
+        print("\n\nmu: {}".format(mu.shape))
+        print("\n\nx: {}".format(x))
+        print("\n\nx: {}".format(x.shape))
+        print("\n\nsigma: {}".format(sigma))
+        print("\n\nsigma: {}".format(sigma.shape))
+        size = len(x)
+        if size == len(mu) and (size, size) == sigma.shape:
+            det = np.linalg.det(sigma)
+            if det == 0:
+                raise NameError("The covariance matrix can't be singular")
+
+            norm_const = 1.0 / (math.pow((2 * math.pi), float(size) / 2) * math.pow(det, 1.0 / 2))
+            x_mu = np.matrix(x - mu)
+            inv = sigma.I
+            part1 = (x_mu * inv * x_mu.T)
+            print("\n\npart 1:\n{}".format(part1))
+            result = math.pow(math.e, -0.5 * (x_mu * inv * x_mu.T))
+            L = norm_const * result
+            return L
+        else:
+            raise NameError("The dimensions of the input don't match")
--- tracktree.py
+++ tracktree.py
@@ -6,8 +6,10 @@ class TrackTree:
     """
     def __init__(self, frame_number):
         self.frame_number = frame_number
+        self.image_area = 10.0  # Placeholder
         self.nodes = []
         self.kf = None  # Kalman filter for motion scores
+        self.motion_score = 1.0
 
     def get_frame_number(self):
         return self.frame_number
@@ -15,12 +17,17 @@ class TrackTree:
     def add_detection(self, detection):
         if self.nodes:
             if detection is not None:  # Missing detections are ignored / not updated with the Kalman filter
-                self.kf.update(detection)  # Update the Kalman filter with the new detection
+                print("Detection shape: {}".format(detection.shape))
+                motion_score = self.kf.update(detection)  # Update the Kalman filter with the new detection
+                print("motion score: {}".format(motion_score))
+                self.motion_score *= (motion_score/(1.0/self.image_area))  # Update the overall motion score
         else:
             self.kf = KalmanFilter(detection)  # Initialize the Kalman filter
         self.nodes.append(detection)
 
     def print_data(self):
-        print("\nTrack tree starting at frame {}".format(self.frame_number))
+        print("\nTrack tree starting at frame {}\n".format(self.frame_number))
         for n in self.nodes:
             print(n)
+
+        print("\nFinal score: {}".format(round(self.motion_score, 3)))
