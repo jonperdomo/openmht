1af42dc656450e5e727097b5ce44bda904846139
Jonathan Perdomo <perdomojonathan5@gmail.com>
2018-06-15 03:10:17 -0400

    Refactor track trees as graphs with vertices

--- openmht.py
+++ openmht.py
@@ -14,6 +14,8 @@ class OpenMHT:
         self.frame_count, _, self.dimensionality = detections.shape
         self.frame_number = 0
         self.track_trees = []  # Track hypotheses for detections in each frame
+        # self.vertex_ids = []  # Assign a vertex ID for each detection (A-Z)
+        self.detection_count = 0
 
     def global_hypothesis(self, trees):
         pass
@@ -21,8 +23,22 @@ class OpenMHT:
     def get_detections(self):
         return self.detections.pop()
 
+    # def get_vertex_id(self):
+    #     ord_index = ord('a') + len(self.vertex_ids)
+    #     ord_length = ord('z') - ord('a')
+    #     ord_factor = int((ord_index-ord('a')) / float(ord_length))
+    #     ord_index -= (ord_length * ord_factor)
+    #     vertex_id = chr(ord_index) * (ord_factor+1)
+    #
+    #     return vertex_id
+
     def run(self):
         print("Number of frames: {}".format(len(self.detections)))
+        # for i in range(200):
+        #     vid = self.get_vertex_id()
+        #     self.vertex_ids.append(vid)
+        #     print(vid)
+
         while self.detections:
             self.frame_number += 1
             detections = self.detections.pop()
@@ -32,20 +48,25 @@ class OpenMHT:
             updated_track_trees = []
             for track_tree in self.track_trees:
                 # Generate updated track trees from the detections
-                for detection in detections:
+                for i in range(len(detections)):
+                    detection, vertex_id = detections[i], str(self.detection_count + i)
                     track_tree_copy = deepcopy(track_tree)
-                    track_tree_copy.add_detection(detection)
+                    track_tree_copy.add_detection(detection, vertex_id)
                     updated_track_trees.append(track_tree_copy)
 
                 # Add a dummy observation to account for missing detections
-                track_tree.add_detection(None)
+                track_tree.add_detection(None, str(self.detection_count + len(detections)))
             self.track_trees.extend(updated_track_trees)
 
             # Generate new track trees from the detections
-            for detection in detections:
+            for i in range(len(detections)):
+                detection, vertex_id = detections[i], str(self.detection_count + i)
                 track_tree = TrackTree(self.frame_number)
-                track_tree.add_detection(detection)
+                track_tree.add_detection(detection, vertex_id)
                 self.track_trees.append(track_tree)
+
+            self.detection_count += len(detections) + 1  # +1 for the missing detection ID
+
         print("Done")
         for i in range(len(self.track_trees)):
             # print("\nTrack {}".format(i))
--- tracktree.py
+++ tracktree.py
@@ -1,4 +1,5 @@
 from kalmanfilter import KalmanFilter
+from graph import Graph
 
 class TrackTree:
     """
@@ -10,11 +11,14 @@ class TrackTree:
         self.nodes = []
         self.kf = None  # Kalman filter for motion scores
         self.motion_score = 1.0
+        self.graph = Graph()
+        self.last_vertex_id = ""
 
     def get_frame_number(self):
         return self.frame_number
 
-    def add_detection(self, detection):
+    def add_detection(self, detection, vertex_id):
+        print("VID: ", vertex_id)
         if self.nodes:
             if detection is not None:  # Missing detections are ignored / not updated with the Kalman filter
                 print("Detection shape: {}".format(detection.shape))
@@ -25,9 +29,21 @@ class TrackTree:
             self.kf = KalmanFilter(detection)  # Initialize the Kalman filter
         self.nodes.append(detection)
 
+        # Update the graph
+        self.graph.add_vertex(vertex_id)
+        if detection is None:
+            self.last_vertex_id = ""
+        else:
+            if self.last_vertex_id:
+                self.graph.add_edge({self.last_vertex_id, vertex_id})  # Continued vertex path
+
+            self.last_vertex_id = vertex_id
+
     def print_data(self):
         print("\nTrack tree starting at frame {}\n".format(self.frame_number))
         for n in self.nodes:
             print(n)
 
         print("\nFinal score: {}".format(round(self.motion_score, 3)))
+        print("\nFinal graph:\n")
+        print(self.graph)
