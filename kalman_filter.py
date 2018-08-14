BUG: Fix the log likelihood computation

kalman_filter.py
@@ -10,45 +10,87 @@ class KalmanFilter:
         # For 2D X & Y pixel positions
         # print("initial observation: {}".format(initial_observation))
         # print("initial observation shape: {}".format(np.matrix(initial_observation).T.shape))
-        self.z = [np.matrix(initial_observation).T]  # observations
-        self.Q = np.matrix(np.eye(2)) * 1e-5
+        self.dims = 2
+
+        x = np.matrix(initial_observation).reshape((self.dims, 1))
+        self.z = [x]  # observations
+        self.Q = np.matrix(np.eye(self.dims)) * 1e-5
 
         # allocate space for arrays
-        self.xhat = []  # a posteri estimate of x
-        self.P = [np.matrix(np.eye(2))]  # a posteri error estimate
+        self.xhat = [x]  # a posteri estimate of x
+        self.P = [np.matrix(np.eye(self.dims))]  # a posteri error estimate        # print("\nActual position: {}".format(z))
+        # print("\nCovariance (sigma): {}".format(self.P[-1]))
+
+        # get the likelihood score
+        mu = self.xhat[-1]
+        sigma = self.P[-1]
+
+        L = self.calculate_likelihood(x, mu, sigma)
+
         self.xhatminus = []  # a priori estimate of x
         self.Pminus = []  # a priori error estimate
         self.K = []  # gain or blending factor
         self.R = 0.1 ** 2  # estimate of measurement variance, change to see effect
 
-        # initial guesses
-        self.xhat.append(self.z[-1])
-
     def update(self, z):
-        self.z.append(z)
-        # print("\nPredicted position (mu): {}".format(self.xhat[-1]))
-        # print("\nActual position: {}".format(z))
-        # print("\nCovariance (sigma): {}".format(self.P[-1]))
-
-        # get the likelihood score
-        L = self.calculate_likelihood(z, self.xhat[-1], self.P[-1])
-
-        # time update
-        self.xhatminus.append(self.xhat[-1])
-        self.Pminus.append((self.P[-1] + self.Q))
-
-        # measurement update
-        self.K.append(self.Pminus[-1] / (self.Pminus[-1] + self.R))
-        self.xhat.append(self.xhatminus[-1] + self.K[-1] * (self.z[-1] - self.xhatminus[-1]))
-        self.P.append((1.0 - self.K[-1]) * self.Pminus[-1])
-
-        return L
+        pass
+        # x = np.matrix(z).reshape((self.dims, 1))
+        # self.z.append(x)
+        # # print("\nPredicted position (mu): {}".format(self.xhat[-1]))
+        #
+        # # time update
+        # self.xhatminus.append(mu)
+        # Pminus = sigma + self.Q
+        # self.Pminus.append(Pminus)
+        #
+        # # measurement update
+        # K = sigma / (sigma + self.R)
+        # self.K.append(K)
+        #
+        # xhat_a = mu + K
+        # print("a shape: {}".format(xhat_a.shape))
+        # xhat_b = x - mu
+        # print("b shape: {}".format(xhat_b.shape))
+        # xhat = np.matrix(xhat_a) * np.matrix(xhat_b).reshape((self.dims, 1))
+        # self.xhat.append(xhat)
+        #
+        # P = (1.0 - K) * Pminus
+        # self.P.append(P)
+        #
+        # # self.xhat.append(self.xhatminus[-1] + self.K[-1] * (self.z[-1] - self.xhatminus[-1]))  # Check outputs here
+        # # self.P.append((1.0 - self.K[-1]) * self.Pminus[-1])
+        #
+        # return L
 
     def calculate_likelihood(self, x, mu, sigma):
         """
         Likelihood function for a multivariate normal distribution.
         https://en.wikipedia.org/wiki/Multivariate_normal_distribution#Likelihood_function
+        sigma: k x k covariance matrix
+        x: single observation
+        mu: k-dimensional mean vector
         """
+        assert x.shape == (self.dims, 1), "X shape did not match dimensions {}".format(x.shape)
+        assert mu.shape == (self.dims, 1), "Mu shape did not match dimensions {}".format(mu.shape)
+        assert sigma.shape == (self.dims, self.dims), "Sigma shape did not match dimensions {}".format(sigma.shape)
+        print("Shapes: x: {}, mu: {}, sigma: {}".format(x, mu, sigma))
+
+        _, evec_sigma = np.linalg.eig(np.abs(sigma))
+        ln_sigma = np.linalg.inv(evec_sigma)
+        np.fill_diagonal(ln_sigma, np.log(ln_sigma.diagonal()))
+
+        op2 = (x-mu).T
+
+        op3 = op2 * np.linalg.inv(sigma)
+
+        op4 = (x-mu)
+
+        op5 = op3 * op4
+
+        op6 = 3. * np.log(np.pi)
+
+        op7 = op5 + op6
+
         x = np.squeeze(x)
         mu = np.array(mu).ravel('F')
         if mu.shape != (2,):
@@ -66,6 +108,35 @@ class KalmanFilter:
             inv = sigma.I
             result = math.pow(math.e, -0.5 * (x_mu * inv * x_mu.T))
             L = norm_const * result
+            print("L: {}".format(L))
+            return L
+        else:
+            raise NameError("The dimensions of the input don't match")
+
+    def calculate_likelihood_DEPR(self, x, mu, sigma):
+        """
+        Likelihood function for a multivariate normal distribution.
+        https://en.wikipedia.org/wiki/Multivariate_normal_distribution#Likelihood_function
+        """
+        print("Shapes: x: {}, mu: {}, sigma: {}".format(x, mu, sigma))
+        x = np.squeeze(x)
+        mu = np.array(mu).ravel('F')
+        if mu.shape != (2,):
+            return 1.0
+
+        sigma = np.matrix(sigma).reshape((2, 2))
+        size = len(x)
+        if size == len(mu) and (size, size) == sigma.shape:
+            det = np.linalg.det(sigma)
+            if det == 0:
+                raise NameError("The covariance matrix can't be singular")
+
+            norm_const = 1.0 / (math.pow((2 * math.pi), float(size) / 2) * math.pow(det, 1.0 / 2))
+            x_mu = np.matrix(x - mu)
+            inv = sigma.I
+            result = math.pow(math.e, -0.5 * (x_mu * inv * x_mu.T))
+            L = norm_const * result
+            print("L: {}".format(L))
             return L
         else:
             raise NameError("The dimensions of the input don't match")

test.py
@@ -10,7 +10,7 @@ def get_test_detections():
     """
     :return: An array of randomly-generated detections (ijk) for each frame.
     """
-    frame_count = 10
+    frame_count = 3
     dimensionality = 2
 
     # Truth values for 3 objects
@@ -19,7 +19,7 @@ def get_test_detections():
     for i in range(len(truth_values)):
         detections[:, i] = np.random.normal(truth_values[i], 0.1, size=(frame_count, dimensionality))
 
-    print("Detections: {}".format(detections))
+    # print("Detections: {}".format(detections))
     return detections
 
 if __name__ == "__main__":